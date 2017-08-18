ac7ab69041ab0e079777a10cc54c358d5a309c1b
Tushar-d-p <31070316+Tushar-d-p@users.noreply.github.com>
2017-08-18 18:08:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-    res.send('Aricle two resuqsted and will be served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
     
 });
 
 app.get('/article-three',function(req,res){
-    res.send('Aricle three resuqsted and will be served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
     
 });
 
