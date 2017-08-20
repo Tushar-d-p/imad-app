83554be5bf75549f611d85d73530ffcb0708f067
Tushar-d-p <31070316+Tushar-d-p@users.noreply.github.com>
2017-08-20 17:29:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,29 @@ var counter=0;
 
 button.onclick = function () {
     
+    
+    var request = new XMLHttpRequest();
+    
+    
+    
+    
+    request.onreadystatechange = function()
+    {
+        if(request.readyState == XMLHttpRequest.DONE)
+        {
+            if(request.status == 200)
+            {
+                var counter= request.responseText;
+            }
+        }
+        
+        
+        
+    };
+    
+    
+    
+    
     //render the variable in correct span
     counter=counter+1;
     var span= document.getElementById('count');
