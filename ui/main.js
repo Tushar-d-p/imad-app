74f435f486c37066d66c35acc2320badaa4eaf1f
Tushar-d-p <31070316+Tushar-d-p@users.noreply.github.com>
2017-08-20 15:15:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,11 @@ var element = document.getElementById('main-text'
     );
 
 element.innerHTML= 'New Value';
+
+// move the image
+
+var img=document.getElementById('img');
+img.onclick= function() {
+    img.style.marginleft='50px';
+};
+
