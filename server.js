dc2f5d5046fdb58a6a34935f16001460fb5e880e
Tushar-d-p <31070316+Tushar-d-p@users.noreply.github.com>
2017-08-23 17:47:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,20 +126,20 @@ app.get('/submit-name', function(req,res){//URL :/submit-name?name=xxx
 app.get('/articles/:articleName',function(req,res){
     
     
-    pool.query("SELECT * FROM article WHERE title= '"+ req.params.articleName+ " ' ",function(err,result){
+    pool.query("SELECT * FROM article WHERE title= '" + req.params.articleName+ " ' ", function(err,result){
        if(err){
            req.status(500).send(err.toString());
        } 
        else
        {
-           if(result.rows.length===0)
+           if(result.rows.length === 0)
            {
                res.status(404).send("article not found");
                
            }
            else
            {
-               var articleData = results.rows [0];
+               var articleData = result.rows [0];
                res.send(createTemplate(articleData));
            }
        }
