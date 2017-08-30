c26f6eb49a18c7a669f5b7ece1e2b64cdbdba97f
Tushar-d-p <31070316+Tushar-d-p@users.noreply.github.com>
2017-08-30 16:55:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,36 +1,6 @@
-var button= document.getElementById('counter');
 
 
-
-button.onclick = function () {
-    
-    
-    var request = new XMLHttpRequest();
-    
-    
-    
-    //capture the respone
-    request.onreadystatechange = function()
-    {
-        if(request.readyState == XMLHttpRequest.DONE)
-        {
-            if(request.status == 200)
-            {
-                var counter= request.responseText;
-                var span= document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        
-        
-        
-    };
-    //make the request
-    request.open('GET','http://algorithm321.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-
-
+// submit username/password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function (){
   // make request to server and send the name
@@ -43,16 +13,22 @@ submit.onclick = function (){
         {
             if(request.status == 200)
             {
-                var names=request.responseText;
-                names = JSON.parse(names);
-                  var list = '';
-                  for(var i=0;i<names.length;i++)
-                  {
-                      list+= '<li>'+ names[i] + '</li>';
-                  }
-                  var ul= document.getElementById('namelist');
-                  ul.innerHTML=list;
-                  
+                console.log('user logged in');
+                alert('logged in successfully');
+            }
+            else
+            {
+                if(request.status == 403)
+                {
+                    alert('username/password is incorrect');
+                }
+                else
+                {
+                    if(request.status == 500)
+                    {
+                        alert('something went wrong');
+                    }
+                }
             }
         }
         
@@ -60,11 +36,14 @@ submit.onclick = function (){
         
     };
     
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
+    var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+     console.log(username);
+     console.log(password);
     //make the request
-    request.open('GET','http://algorithm321.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    request.open('POST','http://algorithm321.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
     
     // capture names of list and rendered it in list
   
